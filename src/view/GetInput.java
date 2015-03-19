dcf80064536213079b66ec9730cbe3b48348064b
unknown <rjb12180@CIS-TELFORD-03.ds.strath.ac.uk>
2015-03-19 15:20:01 +0000

    update load/save to use filepicker

--- src/view/GetInput.java
+++ src/view/GetInput.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import model.Model;
@@ -7,15 +10,29 @@ import model.Model;
 public class GetInput {
 
 	String input;
-	
+	JFileChooser fc;
+
 	public GetInput() {
-		
+		fc = new JFileChooser();
+	}
+
+	public String showSavePopup(String message, Model m) {
+		fc.showSaveDialog(null);
+		File file = fc.getSelectedFile();
+		// This is where a real application would open the file.
+		input = file.getPath();
+		// input =JOptionPane.showInputDialog(message);
+		return input;
+
 	}
 	
-	public String showPopup(String message, Model m) {
-		//buildPopup();
-		input =JOptionPane.showInputDialog(message);
+	public String showOpenPopup(String message, Model m) {
+		fc.showOpenDialog(null);
+		File file = fc.getSelectedFile();
+		// This is where a real application would open the file.
+		input = file.getPath();
+		// input =JOptionPane.showInputDialog(message);
 		return input;
-		
+
 	}
 }
